6adf6699b89f5b2d4dfd65bf8f60b585a36b48ad
Halil DİKİŞLİ <halil851@gmail.com>
2023-02-22 17:43:19 +0300

    Initial Commit

---
+++ MVVM MovieUITests/MVVM_MovieUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MVVM_MovieUITestsLaunchTests.swift
+//  MVVM MovieUITests
+//
+//  Created by halil dikişli on 22.02.2023.
+//
+
+import XCTest
+
+final class MVVM_MovieUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
